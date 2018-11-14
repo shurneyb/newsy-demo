7da03e411c82f32bd5daca832a469ee216d34861
shurneyb <shurneyb@gmail.com>
2018-11-14 12:36:51 -0500

    Git Time

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  console.log('scripts loaded');
+
+  var myKey = config.MY_KEY;
+  var url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=7abd3bf30afe43fa9a73b2a6257c0f4a' + myKey;
+
+});
